f63c3c466212cedfec69e01d5f347f3d134ccd2f
Tim Millican <tim.millican@gmail.com>
2020-03-27 12:24:53 -0400

    Fix label formatting

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "linode" {
 
 resource "linode_instance" "hashi-masters" {
     count = 1
-    label = "hashi-master-${format("%2d", count.index)}"
+    label = "hashi-master-${format("%02d", count.index + 1)}"
     image = "linode/gentoo"
     region = "us-east"
     type = "g6-standard-1"
