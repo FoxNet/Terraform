f47c98b8668a141861eb9b5461359980502d77fb
Tim Millican <tim.millican@gmail.com>
2020-03-26 23:13:14 -0400

    Add single Linode Instance

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+provider "linode" {
+  token = "${var.linode_api_key}"
+}
 
+resource "linode_instance" "hashi-masters" {
+    for_each = ["01"]
+    label = "hashi-master-${each.value}"
+    image = "linode/gentoo"
+    region = "us-east"
+    type = "g6-standard-1"
+    root_pass = "terr4form-test"
+
+    group = "hashicorp"
+    tags = [ "hashi-master" ]
+    swap_size = 256
+    private_ip = true
+}
